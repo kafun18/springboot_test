5c564ea645b62dd0652c1447464f8f08fbda948f
kafun18 <513112374@qq.com>
2019-10-21 03:12:43 +0800

    springboot登陆功能

--- demovhr/src/main/java/org/javaboy/vhr/DemovhrApplication.java
+++ demovhr/src/main/java/org/javaboy/vhr/DemovhrApplication.java
@@ -1,9 +1,11 @@
 package org.javaboy.vhr;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "org.javaboy.vhr.mapper")
 public class DemovhrApplication {
 
 	public static void main(String[] args) {
