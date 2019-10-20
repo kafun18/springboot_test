8e5895b3351fdfde4a1011c0c6b693c6815005fd
kafun18 <513112374@qq.com>
2019-10-21 00:12:56 +0800

    上传服务端

---
+++ demovhr/src/main/java/org/javaboy/vhr/DemovhrApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.vhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemovhrApplication.class, args);
+	}
+
+}
